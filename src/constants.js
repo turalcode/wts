732c0fc923c48966b97b00725dda9abbedcbc09b
turalcode <turalcodechannel@gmail.com>
2026-01-04 20:24:55 +0300

    добавил 2026 год

--- src/constants.js
+++ src/constants.js
@@ -1,93 +1,141 @@
 export const MONTHS = [
-    {name: "Январь", value: 0},
-    {name: "Февраль", value: 1},
-    {name: "Март", value: 2},
-    {name: "Апрель", value: 3},
-    {name: "Май", value: 4},
-    {name: "Июнь", value: 5},
-    {name: "Июль", value: 6},
-    {name: "Август", value: 7},
-    {name: "Сентябрь", value: 8},
-    {name: "Октябрь", value: 9},
-    {name: "Ноябрь", value: 10},
-    {name: "Декабрь", value: 11}
+  { name: "Январь", value: 0 },
+  { name: "Февраль", value: 1 },
+  { name: "Март", value: 2 },
+  { name: "Апрель", value: 3 },
+  { name: "Май", value: 4 },
+  { name: "Июнь", value: 5 },
+  { name: "Июль", value: 6 },
+  { name: "Август", value: 7 },
+  { name: "Сентябрь", value: 8 },
+  { name: "Октябрь", value: 9 },
+  { name: "Ноябрь", value: 10 },
+  { name: "Декабрь", value: 11 },
 ];
 
 export const YEARS = [
-    {name: "2024", value: 2024},
-    {name: "2025", value: 2025},
-    {name: "2026", value: 2026},
-    {name: "2027", value: 2027},
-    {name: "2028", value: 2028}
+  { name: "2024", value: 2024 },
+  { name: "2025", value: 2025 },
+  { name: "2026", value: 2026 },
+  { name: "2027", value: 2027 },
+  { name: "2028", value: 2028 },
 ];
 
 export const MONTHS_PARAMETERS = {
-    "2024.7": {
-        workingDays: 22,
-        appVersion: 1.1
-    },
-    "2024.8": {
-        workingDays: 22,
-        appVersion: 1.1
-    },
-    "2024.9": {
-        workingDays: 22,
-        appVersion: 1.1
-    },
-    "2024.10": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2024.11": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.0": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.1": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.2": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.3": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.4": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.5": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.6": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.7": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.8": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.9": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.10": {
-        workingDays: 22,
-        appVersion: 1.2
-    },
-    "2025.11": {
-        workingDays: 22,
-        appVersion: 1.2
-    }
+  2024.7: {
+    workingDays: 22,
+    appVersion: 1.1,
+  },
+  2024.8: {
+    workingDays: 22,
+    appVersion: 1.1,
+  },
+  2024.9: {
+    workingDays: 22,
+    appVersion: 1.1,
+  },
+  "2024.10": {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2024.11: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  "2025.0": {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.1: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.2: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.3: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.4: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.5: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.6: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.7: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.8: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.9: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  "2025.10": {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2025.11: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  "2026.0": {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.1: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.2: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.3: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.4: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.5: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.6: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.7: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.8: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.9: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  "2026.10": {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
+  2026.11: {
+    workingDays: 22,
+    appVersion: 1.2,
+  },
 };
--- src/pages/EmployeePage.jsx
+++ src/pages/EmployeePage.jsx
@@ -1,277 +1,259 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useAuth} from "../hooks/useAuth";
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {MONTHS} from "../constants";
-import {useCheckVersion} from "../hooks/useVersion";
-import {updateEmployee} from "../store/employeesSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { MONTHS } from "../constants";
+import { useCheckVersion } from "../hooks/useVersion";
+import { updateEmployee } from "../store/employeesSlice";
 import MonthSalary from "../components/MonthSalary";
 import imageRefresh from "../../public/images/refresh.png";
 
 const EmployeePage = () => {
-    const {isAuth} = useAuth();
-    const navigate = useNavigate();
-    const {id} = useParams();
-    const employees = useSelector((state) => state.employees.employees);
-    const dispatch = useDispatch();
-    const [employeeName, setEmployeeName] = useState("");
-    const [employeeSalary, setEmployeeSalary] = useState(0);
-    const [phone, setPhone] = useState("");
-    const [telegramID, setTelegramID] = useState("");
-    const [salaryReport, setSalaryReport] = useState([]);
+  const { isAuth } = useAuth();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const employees = useSelector((state) => state.employees.employees);
+  const dispatch = useDispatch();
+  const [employeeName, setEmployeeName] = useState("");
+  const [employeeSalary, setEmployeeSalary] = useState(0);
+  const [phone, setPhone] = useState("");
+  const [telegramID, setTelegramID] = useState("");
+  const [salaryReport, setSalaryReport] = useState([]);
 
-    useEffect(() => {
-        if (!isAuth) return navigate("/wts");
+  useEffect(() => {
+    if (!isAuth) return navigate("/wts");
 
-        const employee = employees.find((employee) => employee.id === id);
+    const employee = employees.find((employee) => employee.id === id);
 
-        if (employee) {
-            setSalaryReportHandler(employee);
-            setEmployeeName(employee.name);
-            setEmployeeSalary(employee.salary);
-            setPhone(employee.phone || "");
-            setTelegramID(employee.telegramID || "");
-        }
-    }, []);
-
-    function salaryReportCalculation(employee) {
-        // Сортировка ключей
-        const keys = Object.keys(employee.dates).sort(
-            (a, b) => +b.replace(".", "") - +a.replace(".", "")
-        );
-
-        const report = [];
-
-        for (const key of keys) {
-            const month = employee.dates[key];
-
-            // Поддерживает ли месяц возможность отчета о зарплате
-            if (!useCheckVersion.isSalaryReport(key)) continue;
-            if (month.daysWorkedPerMonth === 0) continue;
-
-            // Месячная зарплата
-            let salary = 0;
-            // Сверхурочные за месяц
-            let overtime = 0;
-            // Общая сумма за месяц
-            let total = 0;
-            // Количество рабочих дней в месяце
-            // const numberWorkingDays = MONTHS_PARAMETERS[key].workingDays;
-            const numberWorkingDays = 22;
-
-            month.days.forEach((number) => {
-                const day = month[number];
-
-                if (
-                    day.isWorked &&
-                    month.salary > 0 &&
-                    day.overtimeWork >= 0 &&
-                    day.overtimeRatio > 0
-                ) {
-                    // Стоимость одного часа работы
-                    const costOfOneHourOfWork =
-                        month.salary / numberWorkingDays / day.workShift;
-                    // Стоимость одного часа сверхурочной работы
-                    const costHourOvertimeWork =
-                        costOfOneHourOfWork * day.overtimeRatio;
-
-                    // Подсчет зарплаты
-                    if (useCheckVersion.isDayOff(key)) {
-                        if (day.isDayOff) {
-                            overtime +=
-                                costHourOvertimeWork * day.hoursWorkedPerDay;
-                        } else {
-                            salary +=
-                                costOfOneHourOfWork *
-                                (day.hoursWorkedPerDay - day.overtimeWork);
-                            overtime += costHourOvertimeWork * day.overtimeWork;
-                        }
-                    } else {
-                        if (
-                            day.dayOfTheWeek === "Сб" ||
-                            day.dayOfTheWeek === "Вс"
-                        ) {
-                            overtime +=
-                                costHourOvertimeWork * day.hoursWorkedPerDay;
-                        } else {
-                            salary +=
-                                costOfOneHourOfWork *
-                                (day.hoursWorkedPerDay - day.overtimeWork);
-                            overtime += costHourOvertimeWork * day.overtimeWork;
-                        }
-                    }
-
-                    total = salary + overtime;
-                }
-            });
-
-            report.push({
-                month: {
-                    date: modifyDate(key),
-                    salary: salary.toFixed(2),
-                    overtime: overtime.toFixed(2),
-                    total: total.toFixed(2),
-                    monthSalary: month.salary,
-                    numberWorkingDays,
-                    key
-                },
-                employeeId: id
-            });
-        }
-
-        return report;
+    if (employee) {
+      setSalaryReportHandler(employee);
+      setEmployeeName(employee.name);
+      setEmployeeSalary(employee.salary);
+      setPhone(employee.phone || "");
+      setTelegramID(employee.telegramID || "");
     }
+  }, []);
 
-    function modifyDate(date) {
-        if (date) {
-            const index = +date.slice(5);
-            return `${date.slice(0, 4)} - ${MONTHS[index].name}`;
-        }
-    }
+  function salaryReportCalculation(employee) {
+    // Сортировка ключей
+    const keys = Object.keys(employee.dates).sort(
+      (a, b) => +b.replace(".", "") - +a.replace(".", "")
+    );
 
-    async function updateEmployeeHandler(event) {
-        event.preventDefault();
-        let name = employeeName.slice(0, 10).trim();
+    const report = [];
+
+    for (const key of keys) {
+      const month = employee.dates[key];
+
+      // Поддерживает ли месяц возможность отчета о зарплате
+      if (!useCheckVersion.isSalaryReport(key)) continue;
+      if (month.daysWorkedPerMonth === 0) continue;
+
+      // Месячная зарплата
+      let salary = 0;
+      // Сверхурочные за месяц
+      let overtime = 0;
+      // Общая сумма за месяц
+      let total = 0;
+      // Количество рабочих дней в месяце
+      // const numberWorkingDays = MONTHS_PARAMETERS[key].workingDays;
+      const numberWorkingDays = 22;
+
+      month.days.forEach((number) => {
+        const day = month[number];
 
         if (
-            !name ||
-            !Number.isInteger(+employeeSalary) ||
-            +employeeSalary <= 0
+          day.isWorked &&
+          month.salary > 0 &&
+          day.overtimeWork >= 0 &&
+          day.overtimeRatio > 0
         ) {
-            return alert("Поле 'Имя' или 'Оклад' содержит ошибку");
+          // Стоимость одного часа работы
+          const costOfOneHourOfWork =
+            month.salary / numberWorkingDays / day.workShift;
+          // Стоимость одного часа сверхурочной работы
+          const costHourOvertimeWork = costOfOneHourOfWork * day.overtimeRatio;
+
+          // Подсчет зарплаты
+          if (useCheckVersion.isDayOff(key)) {
+            if (day.isDayOff) {
+              overtime += costHourOvertimeWork * day.hoursWorkedPerDay;
+            } else {
+              salary +=
+                costOfOneHourOfWork *
+                (day.hoursWorkedPerDay - day.overtimeWork);
+              overtime += costHourOvertimeWork * day.overtimeWork;
+            }
+          } else {
+            if (day.dayOfTheWeek === "Сб" || day.dayOfTheWeek === "Вс") {
+              overtime += costHourOvertimeWork * day.hoursWorkedPerDay;
+            } else {
+              salary +=
+                costOfOneHourOfWork *
+                (day.hoursWorkedPerDay - day.overtimeWork);
+              overtime += costHourOvertimeWork * day.overtimeWork;
+            }
+          }
+
+          total = salary + overtime;
         }
+      });
 
-        if (name.length >= 10) name += "...";
-
-        dispatch(
-            updateEmployee({
-                id,
-                name: employeeName,
-                salary: +employeeSalary,
-                phone,
-                telegramID
-            })
-        );
+      report.push({
+        month: {
+          date: modifyDate(key),
+          salary: salary.toFixed(2),
+          overtime: overtime.toFixed(2),
+          total: total.toFixed(2),
+          monthSalary: month.salary,
+          numberWorkingDays,
+          key,
+        },
+        employeeId: id,
+      });
     }
 
-    function setSalaryReportHandler(employee) {
-        const report = salaryReportCalculation(employee);
-        setSalaryReport(report);
+    return report;
+  }
+
+  function modifyDate(date) {
+    if (date) {
+      const index = +date.slice(5);
+      return `${date.slice(0, 4)} - ${MONTHS[index].name}`;
+    }
+  }
+
+  async function updateEmployeeHandler(event) {
+    event.preventDefault();
+    let name = employeeName.slice(0, 10).trim();
+
+    if (!name || !Number.isInteger(+employeeSalary) || +employeeSalary <= 0) {
+      return alert("Поле 'Имя' или 'Оклад' содержит ошибку");
     }
 
-    return (
-        <>
-            <form className="mb-5 pb-4 flex bg-slate-100">
-                <fieldset className="flex">
-                    <label className="p-2 bg-slate-100">Имя:</label>
-                    <input
-                        onChange={(e) => setEmployeeName(e.target.value)}
-                        value={employeeName}
-                        className="px-2 text-xl rounded-lg"
-                        placeholder="Имя сотрудника"
-                    />
-                </fieldset>
+    if (name.length >= 10) name += "...";
 
-                <fieldset className="ml-4 flex">
-                    <label className="p-2 bg-slate-100">Оклад:</label>
-                    <input
-                        onChange={(e) => setEmployeeSalary(e.target.value)}
-                        value={employeeSalary}
-                        className="px-2 rounded-lg"
-                        placeholder="Текущий оклад"
-                    />
-                </fieldset>
-
-                <fieldset className="ml-4 flex">
-                    <label className="p-2 bg-slate-100">Телефон:</label>
-                    <input
-                        onChange={(e) => setPhone(e.target.value)}
-                        value={phone}
-                        className="px-2 rounded-lg"
-                        placeholder="+7(999)999-99-99"
-                    />
-                </fieldset>
-
-                <fieldset className="ml-4 flex">
-                    <label className="p-2 bg-slate-100">Telegram ID:</label>
-                    <input
-                        onChange={(e) => setTelegramID(e.target.value)}
-                        value={telegramID}
-                        className="px-2 rounded-lg"
-                        placeholder="ID"
-                    />
-                </fieldset>
-
-                <button
-                    onClick={updateEmployeeHandler}
-                    className="ml-2 py-2 px-4 bg-green-100 text-sm tracking-wide rounded-lg"
-                >
-                    <img
-                        className="size-7 bg-green-100 rounded-full"
-                        src={imageRefresh}
-                    />
-                </button>
-            </form>
-
-            <table className="w-full text-left rtl:text-right dark:text-gray-400 text-gray-900">
-                <thead className="bg-slate-100">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            Дата
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Оклад
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            По графику
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Переработка
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            К зарплате
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {salaryReport.map((report) => {
-                        return (
-                            <tr
-                                key={report.month.date}
-                                className="odd:bg-white even:bg-slate-50"
-                            >
-                                <td
-                                    scope="row"
-                                    className="px-6 py-4 whitespace-nowrap dark:text-white capitalize"
-                                >
-                                    {report.month.date}
-                                </td>
-                                <td className="px-6 py-4">
-                                    <MonthSalary
-                                        employeeId={report.employeeId}
-                                        monthKey={report.month.key}
-                                        monthSalary={report.month.monthSalary}
-                                        setSalaryReportHandler={
-                                            setSalaryReportHandler
-                                        }
-                                    />
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.month.salary} &#8381;
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.month.overtime} &#8381;
-                                </td>
-                                <td className="px-6 py-4">
-                                    {report.month.total} &#8381;
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </>
+    dispatch(
+      updateEmployee({
+        id,
+        name: employeeName,
+        salary: +employeeSalary,
+        phone,
+        telegramID,
+      })
     );
+  }
+
+  function setSalaryReportHandler(employee) {
+    const report = salaryReportCalculation(employee);
+    setSalaryReport(report);
+  }
+
+  return (
+    <>
+      <form className="mb-5 pb-4 flex bg-slate-100">
+        <fieldset className="flex">
+          <label className="p-2 bg-slate-100">Имя:</label>
+          <input
+            onChange={(e) => setEmployeeName(e.target.value)}
+            value={employeeName}
+            className="px-2 text-xl rounded-lg"
+            placeholder="Имя сотрудника"
+          />
+        </fieldset>
+
+        <fieldset className="ml-4 flex">
+          <label className="p-2 bg-slate-100">Оклад:</label>
+          <input
+            onChange={(e) => setEmployeeSalary(e.target.value)}
+            value={employeeSalary}
+            className="px-2 rounded-lg"
+            placeholder="Текущий оклад"
+          />
+        </fieldset>
+
+        <fieldset className="ml-4 flex">
+          <label className="p-2 bg-slate-100">Телефон:</label>
+          <input
+            onChange={(e) => setPhone(e.target.value)}
+            value={phone}
+            className="px-2 rounded-lg"
+            placeholder="+7(999)999-99-99"
+          />
+        </fieldset>
+
+        <fieldset className="ml-4 flex">
+          <label className="p-2 bg-slate-100">Telegram ID:</label>
+          <input
+            onChange={(e) => setTelegramID(e.target.value)}
+            value={telegramID}
+            className="px-2 rounded-lg"
+            placeholder="ID"
+          />
+        </fieldset>
+
+        <button
+          onClick={updateEmployeeHandler}
+          className="ml-2 py-2 px-4 bg-green-100 text-sm tracking-wide rounded-lg"
+        >
+          <img
+            className="size-7 bg-green-100 rounded-full"
+            src={imageRefresh}
+          />
+        </button>
+      </form>
+
+      <table className="w-full text-left rtl:text-right dark:text-gray-400 text-gray-900">
+        <thead className="bg-slate-100">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Дата
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Оклад
+            </th>
+            <th scope="col" className="px-6 py-3">
+              По графику
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Переработка
+            </th>
+            <th scope="col" className="px-6 py-3">
+              К зарплате
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {salaryReport.map((report) => {
+            return (
+              <tr
+                key={report.month.date}
+                className="odd:bg-white even:bg-slate-50"
+              >
+                <td
+                  scope="row"
+                  className="px-6 py-4 whitespace-nowrap dark:text-white capitalize"
+                >
+                  {report.month.date}
+                </td>
+                <td className="px-6 py-4">
+                  <MonthSalary
+                    employeeId={report.employeeId}
+                    monthKey={report.month.key}
+                    monthSalary={report.month.monthSalary}
+                    setSalaryReportHandler={setSalaryReportHandler}
+                  />
+                </td>
+                <td className="px-6 py-4">{report.month.salary} &#8381;</td>
+                <td className="px-6 py-4">{report.month.overtime} &#8381;</td>
+                <td className="px-6 py-4">{report.month.total} &#8381;</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default EmployeePage;
